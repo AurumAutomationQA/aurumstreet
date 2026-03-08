b38422acea49acb514331aaa2a3d4a9dfd1d4605
Durga Prasad <durga.prasad@wseinfratech.com>
2026-03-08 15:43:15 +0530

    ChangesInPPclass

--- src/test/java/storeTests/PriceProtection.java
+++ src/test/java/storeTests/PriceProtection.java
@@ -1,8 +1,14 @@
 package storeTests;
 
+import org.testng.annotations.BeforeClass;
+
 import utility.ReusableMethods;
 
 public class PriceProtection extends ReusableMethods
 {
-
+	@BeforeClass
+	public void prerequisites() {
+		initialise();
+		
+	}
 }
